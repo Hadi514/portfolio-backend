bb676af9fd1f8540ddb64e24d6d7c89a420168fd
Hadi514 <hadeedullah147@gmail.com>
2024-12-13 15:40:34 +0500

    push again

--- Testingfile.js
+++ Testingfile.js
@@ -1,7 +1,7 @@
 import express from 'express'
 const router = express.Router()
 router.get("/getdata", async(req, res) => {
-    console.log("Chan Chhaiyyan Chaiyyan Chaiyyan Chaiyyan");
+    res.json("Chan Chhaiyyan Chaiyyan Chaiyyan Chaiyyan");
     
 })
 export default router
